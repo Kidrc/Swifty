0e8d2ad0c258608ed7ae9c337a681558d57df7f4
Kidrc <kidrc@KidrctekiMacBook-Air.local>
2015-08-16 15:32:04 +0800

    Lol, return JSON strings

--- BasicUI/ViewController.swift
+++ BasicUI/ViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+struct LSummonerObject {
+    var id:Int = 0
+    var name:String = ""
+    var level:Int = 0
+}
+
 class ViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: Properties
@@ -37,20 +43,59 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
-        lol_Search_By_Name()
+        if let result = lol_Search_By_Name(nameText.text) {
+            dataLabel.text = result.name
+        }
+        else {
+            dataLabel.text = "Summoner Not Found"
+        }
     }
     
     // MARK: Actions
     
     @IBAction func searchButton(sender: UIButton) {
-        lol_Search_By_Name()
+        println(nameText.text)
+        
+        if let result = lol_Search_By_Name(nameText.text) {
+            dataLabel.text = result.name
+        }
+        else {
+            dataLabel.text = "Summoner Not Found"
+        }
     }
-
-    func lol_Search_By_Name() {
-        dataLabel.text = nameText.text;
-    }
-
-
-    
 }
+
+func lol_Search_By_Name(name:String) -> LSummonerObject? {
+    var receivedObject = LSummonerObject()
+    
+    var encodedName = name.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
+    var urlString = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + encodedName + "?api_key=f948774c-1584-4faa-bf93-03f0b344ef9a"
+    var url = NSURL(string: urlString)!
+    
+    var session = NSURLSession.sharedSession()
+    var request = NSMutableURLRequest(URL: url)
+    request.HTTPMethod = "GET"
+    request.addValue("text/html", forHTTPHeaderField: "Content-Type")
+    
+    var completed = false
+    
+    let task = session.dataTaskWithRequest(request) {
+        (data, response, error) -> Void in
+        if error != nil {
+        }
+        else {
+            var dataString = NSString(data: data, encoding: NSUTF8StringEncoding)
+            receivedObject.name = String(dataString!)
+            completed = true
+        }
+    }
+    task.resume()
+    
+    while(!completed) {
+
+    }
+    return receivedObject
+}
+
+
 
